1ee7c539af2bf5f5ff415abdca55a252be2692d4
Abdul-Muktadir <abdullahmuktadir16@gmail.com>
2022-07-20 12:40:48 +0000

    last update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Dashboard=()=>{
+    let navigte=useNavigate();
+    const [auth, setAuth]=useState('');
+    useEffect(()=>{
+      var auth=localStorage.getItem("email");
+      if (auth === null) {
+        navigte('/login');
+      }
+      setAuth(auth);
+    },
+    [])
     return(
         <div>Welcome to Dashboard Page</div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
-import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import {BrowserRouter as Router,Routes,Route,Link, useNavigate} from 'react-router-dom';
 const Header=()=>{
+  // let navigate=useNavigate();
+  const [user, setUser]=useState('');
+  useEffect(()=>{
+    var full_name=localStorage.getItem("full_name");
+    setUser(full_name);
+  },
+  [])
+
+  const LogOut= ()=>{
+    localStorage.removeItem('email');
+    localStorage.clear();
+    // navigate('/login');
+  }
     return(
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
+  <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -19,7 +33,7 @@ const Header=()=>{
       </li>
     </ul>
     <span class="navbar-text">
-      Welcome
+      Welcome:  {user} | <Link to='/login' onClick={LogOut}>LogOut</Link>
     </span>
   </div>
 </nav>
